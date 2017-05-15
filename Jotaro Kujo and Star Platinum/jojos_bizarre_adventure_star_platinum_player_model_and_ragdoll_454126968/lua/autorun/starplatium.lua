2ec0a954e661820f2b802a3be95f97c3a76ee20d
Unknown <j_m_arsenal@ymail.com>
2017-05-15 18:45:29 -0400

    Jotaro Kujo and Star Platinum

---
+++ Jotaro Kujo and Star Platinum/jojos_bizarre_adventure_star_platinum_player_model_and_ragdoll_454126968/lua/autorun/starplatium.lua
@@ -0,0 +1,5 @@
+player_manager.AddValidModel( "Star Platinum", "models/player/jojo3/sp.mdl" )
+
+AddCSLuaFile()
+player_manager.AddValidHands( "Star Platinum", "models/player/jojo3/sparms.mdl", 0, "00000000" )
+
